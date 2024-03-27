612c03ef7a941a9e492731f129ff3e4547f27391
Dainius Zukas <ec22978@qmul.ac.uk>
2024-03-27 11:50:48 +0000

    Update function to handle edge cases

--- Backend/controllers/systemadmin.controller.js
+++ Backend/controllers/systemadmin.controller.js
@@ -4,12 +4,53 @@ const Claim = require('../models/claim.model.js');
 
 
 
-const functionforuser =async (req,res)=>{
-    try{
-
+const CreateAccount = async (req, res) => {
+    try {
+        const { UserID, Password, Expiry, UserType, AccountState, DepartmentID } = req.body;
+        const account = new Account({
+            UserID,
+            Password,
+            Expiry, 
+            UserType, 
+            AccountState, 
+            DepartmentID
+        });
+        const savedAccount = await account.save();
+        res.status(201).json(savedAccount);
+    } catch (err) {
+        res.status(500).json({ message: "An error occured trying to create the account" });
     }
-    catch(err){
+};
 
+const ChangeAccountState = async (req, res) => {
+    try {
+        const { UserID, newState } = req.body;
+        const account = await Account.findById(UserID);
+        if (!account) {
+            return res.status(404).json({ message: "Account not found" });
+        }
+        account.AccountState = newState;
+        const updatedAccount = await account.save();
+        res.status(200).json(updatedAccount);
+    } catch (err) {
+        res.status(500).json({ message: "An error occurred while changing account state" });
+    }
+};
+
+const ViewAllClaims = async (req, res) => {
+    try {
+        const claims = await Claim.find();
+        res.status(200).json(claims);
+    } catch (err) {
+        res.status(500).json({ message: "An error occurred while retrieving claims" });
+    }
+};
+const ViewAllAccounts = async (req, res) => {
+    try {
+        const accounts = await Account.find();
+        res.status(200).json(accounts);
+    } catch (err) {
+        res.status(500).json({ message: "An error occurred while retrieving accounts" });
     }
 };
 
@@ -17,5 +58,8 @@ const functionforuser =async (req,res)=>{
 // enter the function names here
 module.exports = {
     // change this
-    functionforuser
+    CreateAccount,
+    ViewAllClaims,
+    ViewAllAccounts,
+    ChangeAccountState
 }
